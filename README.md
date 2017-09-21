9963c81f5eb2a97caadef460b262fe12b376b797
Stephen Thirlwall <sdt@cpan.org>
2017-09-21 13:39:14 +1000

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Log::Any::Plugin::Color - It's new $module
+
+# SYNOPSIS
+
+    use Log::Any::Plugin::Color;
+
+# DESCRIPTION
+
+Log::Any::Plugin::Color is ...
+
+# LICENSE
+
+Copyright (C) Stephen Thirlwall.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Stephen Thirlwall <sdt@cpan.org>
---
+++ lib/Log/Any/Plugin/Color.pm
@@ -0,0 +1,39 @@
+package Log::Any::Plugin::Color;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Log::Any::Plugin::Color - It's new $module
+
+=head1 SYNOPSIS
+
+    use Log::Any::Plugin::Color;
+
+=head1 DESCRIPTION
+
+Log::Any::Plugin::Color is ...
+
+=head1 LICENSE
+
+Copyright (C) Stephen Thirlwall.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Stephen Thirlwall E<lt>sdt@cpan.orgE<gt>
+
+=cut
+
