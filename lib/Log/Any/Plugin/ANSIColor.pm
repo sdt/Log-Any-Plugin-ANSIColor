e62a905d0f930d5682171e4828f2528ff9a45ff0
Stephen Thirlwall <sdt@cpan.org>
2019-11-07 16:42:52 +1100

    Colorise aliases as well as primary methods
    These should be colored the same as their primary counterparts.
    
    eg. $log->warn(...) should match $log->warning(...)

--- lib/Log/Any/Plugin/ANSIColor.pm
+++ lib/Log/Any/Plugin/ANSIColor.pm
@@ -6,6 +6,9 @@ use warnings;
 our $VERSION = "0.01";
 
 use Log::Any::Plugin::Util  qw( get_old_method set_new_method );
+use Log::Any::Adapter::Util qw(
+    log_level_aliases logging_aliases logging_methods
+);
 use Term::ANSIColor         qw( colored colorvalid );
 
 our %default = (
@@ -28,7 +31,15 @@ sub install {
         }
     }
 
-    for my $method_name (Log::Any->logging_methods) {
+    my %aliases = log_level_aliases();
+    for my $alias (keys %aliases) {
+        my $method = $aliases{$alias};
+
+        $color_map{$alias} = $color_map{$method}
+            if exists $color_map{$method};
+    }
+
+    for my $method_name ( logging_methods(), logging_aliases() ) {
         if (my $color = delete $color_map{$method_name}) {
             # Specifying none as the color name disables colorisation for that
             # method.
